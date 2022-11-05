d076137447e656c59f758c2f78180c0e1573f219
NikitaVasylevich <pryadukhin_n@mail.ru>
2022-11-05 09:52:15 +0700

    Add project files.

---
+++ Function_stepen_2/main.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+
+int Stepen(int a, int b); //Прототип функции//Объявление фукнции
+
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	int a, b;
+	cout << "Введите основание: "; cin >> a;
+	cout << "Введите показатель: "; cin >> b;
+	cout << a << "^" << b << "=" << Stepen(a, b) << endl;   // Вызов функции (Function call)
+
+}
+
+int Stepen(int a, int b) // Реализация функции (Function defenition)
+{
+	int c = 1;
+	for (; b; b--)
+	c = c*a;
+	return c;
+}
+
+
+
